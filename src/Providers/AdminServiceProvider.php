44286f71d7e8a33a9d83529a9aceec44a729a8b1
Joe Jiko <me@joejiko.com>
2016-12-05 15:18:28 -0500

    try service

--- src/Providers/AdminServiceProvider.php
+++ src/Providers/AdminServiceProvider.php
@@ -1,9 +1,28 @@
 <?php namespace Jiko\Admin\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
-class AdminServiceProvider extends ServiceProvider {
-  public function register() {
-    $this->app->register('Jiko\Admin\Providers\RouteServiceProvider');
+class AdminServiceProvider extends ServiceProvider
+{
+  protected $hostArray;
+
+  public function boot()
+  {
+    parent::boot();
+
+    $this->loadViewsFrom(__DIR__ . '/../Admin/views', 'admin');
+    $this->hostArray = ['local.joejiko.com'];
+  }
+
+  public function map()
+  {
+    if (in_array(Input::server('HTTP_HOST'), $this->hostArray)) {
+      require_once(__DIR__ . '/../Http/routes.php');
+    }
+  }
+
+  public function register()
+  {
+
   }
 }
